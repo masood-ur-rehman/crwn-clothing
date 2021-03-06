0082bd4aca82f4551c7ce25e7fd9f368e0139a3e
Masood <masoodrehman39@gmail.com>
2021-03-06 21:23:44 +0500

    lesson 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
-import './homepage.styles.scss';
+import HomePage from './pages/homepage/homepage.component';
+import './pages/homepage/homepage.styles.scss';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
